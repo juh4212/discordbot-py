8e70d8488db41885b26f258018d5cffd4c5cbc17
juh4212 <91859121+juh4212@users.noreply.github.com>
2024-08-06 01:02:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -50,6 +50,7 @@ def save_inventory():
     for item, quantity in inventory.items():
         cursor.execute("REPLACE INTO inventory (item, quantity) VALUES (?, ?)", (item, quantity))
     conn.commit()
+    print("Inventory saved successfully")
 
 def load_prices():
     cursor.execute("SELECT * FROM prices")
@@ -65,6 +66,7 @@ def save_prices():
         cursor.execute("REPLACE INTO prices (item, shoom_price, cash_price) VALUES (?, ?, ?)",
                        (item, price["슘 시세"], price["현금 시세"]))
     conn.commit()
+    print("Prices saved successfully")
 
 @bot.event
 async def on_ready():
@@ -151,8 +153,7 @@ async def show_inventory(interaction: discord.Interaction):
         shoom_price = prices_info["슘 시세"]
         cash_price = prices_info["현금 시세"]
         embed2.add_field(name=item, value=f"재고: {quantity}개\n슘 시세: {shoom_price}슘\n현금 시세: {cash_price}원", inline=True)
-
-        # Items 목록 추가 (세 번째 임베드)
+    # Items 목록 추가 (세 번째 임베드)
     for item in items:
         quantity = inventory.get(item, "N/A")
         prices_info = prices.get(item, {"슘 시세": "N/A", "현금 시세": "N/A"})
@@ -200,3 +201,4 @@ TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 if TOKEN is None:
     raise ValueError("DISCORD_BOT_TOKEN 환경 변수가 설정되지 않았습니다.")
 bot.run(TOKEN)
+
