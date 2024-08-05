81e45593c8d76c920b46baaaf0f15b1200b5cadc
juh4212 <91859121+juh4212@users.noreply.github.com>
2024-08-06 03:01:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -163,7 +163,7 @@ async def show_inventory(interaction: discord.Interaction):
         cash_price = prices_info["현금 시세"]
         embed1.add_field(name=item, value=f"재고: {quantity}개\n슘 시세: {shoom_price}슘\n현금 시세: {cash_price}원", inline=True)
 
-    # Creatures 목록 추가 (두 번째 임베드)
+  # Creatures 목록 추가 (두 번째 임베드)
     for item in creatures[len(creatures)//2:]:
         quantity = inventory.get(item, "N/A")
         prices_info = prices.get(item, {"슘 시세": "N/A", "현금 시세": "N/A"})
@@ -171,14 +171,15 @@ async def show_inventory(interaction: discord.Interaction):
         cash_price = prices_info["현금 시세"]
         embed2.add_field(name=item, value=f"재고: {quantity}개\n슘 시세: {shoom_price}슘\n현금 시세: {cash_price}원", inline=True)
 
-   # Items 목록 추가 (세 번째 임베드)
+    # Items 목록 추가 (세 번째 임베드)
     for item in items:
         quantity = inventory.get(item, "N/A")
         prices_info = prices.get(item, {"슘 시세": "N/A", "현금 시세": "N/A"})
         shoom_price = prices_info["슘 시세"]
         cash_price = prices_info["현금 시세"]
         embed3.add_field(name=item, value=f"재고: {quantity}개\n슘 시세: {shoom_price}슘\n현금 시세: {cash_price}원", inline=True)
-   # 임베드 메시지를 디스코드에 전송
+
+    # 임베드 메시지를 디스코드에 전송
     await interaction.response.send_message(embeds=[embed1, embed2, embed3])
 
 # 슬래시 커맨드: 판매 메시지 생성
